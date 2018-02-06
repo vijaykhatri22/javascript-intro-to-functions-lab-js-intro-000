b8ae99863bdf6b51766ace934f7c0acfc680bbdb
vijay khatri <vijaykhatri22@gmail.com>
2018-02-06 11:41:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ function logWhisper (string){
 
 function sayHiToGrandma (string){
   if (string === "I love you, Grandma."){
-    return "I love you, too."
+    return "I love you, too.";
   }
+  if(string.toUpperCase() === string){
+    return "YES INDEED!";
+  }
+  if(string.toLowerCase() === string){
+    return "I can't hear you!";
+  }
+  
 }
