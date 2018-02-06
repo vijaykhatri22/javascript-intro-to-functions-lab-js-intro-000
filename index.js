e948775ceb6f57a15825316317b68145bc1061bb
vijay khatri <vijaykhatri22@gmail.com>
2018-02-06 11:37:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function logShout (string){
 function logWhisper (string){
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma (string){
+  if (string === "I love you, Grandma."){
+    return "I love you, too."
+  }
+}
